830f8731a56af0525de382dd76797ada83b2b884
George Fleming <v-geflem@microsoft.com>
2016-08-16 08:13:33 -0700

    Add missing copyright

--- src/BufferManipulation.h
+++ src/BufferManipulation.h
@@ -1,3 +1,12 @@
+/*
+**==============================================================================
+**
+** Copyright (c) Microsoft Corporation. All rights reserved. See file LICENSE
+** for license information.
+**
+**==============================================================================
+*/
+
 #ifndef _BufferManipulation_h_
 #define _BufferManipulation_h_
 #include <MI.h>

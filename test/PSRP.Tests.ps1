6bb9d8d51d5b7b27d25567dcac6c3d99956d324c
JumpingYang001 <v-zhenpy@microsoft.com>
2017-03-07 20:31:48 -0800

    Make Pester test more clear and breakdown them into serveral cases

--- test/PSRP.Tests.ps1
+++ test/PSRP.Tests.ps1
@@ -11,7 +11,7 @@ Describe " PowerShell Remoting basic functional tests" -Tag @("CI") {
             $Port = 5986
         }
 
-        It "Remoting from Windows/Linux/MacOS to Linux with basic authentication should work" {
+        It "PowerShell from Windows/Linux/MacOS to Linux with basic authentication over https should work" {
             $hostname = $LinuxHostName
             $User = $LinuxUserName
             $password=$linuxPasswordString
@@ -29,7 +29,7 @@ Describe " PowerShell Remoting basic functional tests" -Tag @("CI") {
             Get-PSSession|Remove-PSSession
         }
 
-        It "Remoting from Windows/Linux/MacOS to Linux with basic authentication with winrm/omicli should work" {
+        It "Remoting from Windows/Linux/MacOS to Linux with basic authentication with omicli over https should work" {
             $hostname = $LinuxHostName
             $User = $LinuxUserName
             if($IsLinux -Or $IsOSX)
@@ -37,15 +37,17 @@ Describe " PowerShell Remoting basic functional tests" -Tag @("CI") {
                 $result = /opt/omi/bin/omicli id -u $User -p $linuxPasswordString --auth Basic --hostname $hostname --port $Port --encryption https
                 $result|Should Not BeNullOrEmpty
             }
-            elseif($IsWindows)
-            {
-                $result = winrm enumerate http://schemas.microsoft.com/wbem/wscim/1/cim-schema/2/OMI_Identify?__cimnamespace=root/omi -r:https://${hostname}:$Port -auth:Basic -u:$User -p:$linuxPasswordString -skipcncheck -skipcacheck -encoding:utf-8
-                # "$result | Should Not BeNullOrEmpty" not works here, it is a Pester bug, so just use Length verificaiton now
-                $result.Length|Should BeGreaterThan 1
-            }
         }
 
-        It "Remoting from Windows/Linux/MacOS to Linux with basic authentication with bad username should throw exception" {
+        It "WSMan from Windows to Linux with basic authentication with winrm over https should work"  -Skip:($IsLinux -Or $IsOSX){
+            $hostname = $LinuxHostName
+            $User = $LinuxUserName
+            $result = winrm enumerate http://schemas.microsoft.com/wbem/wscim/1/cim-schema/2/OMI_Identify?__cimnamespace=root/omi -r:https://${hostname}:$Port -auth:Basic -u:$User -p:$linuxPasswordString -skipcncheck -skipcacheck -encoding:utf-8
+            # "$result | Should Not BeNullOrEmpty" not works here, it is a Pester bug, so just use Length verificaiton now
+            $result.Length|Should BeGreaterThan 1
+        }
+
+        It "PowerShell from Windows/Linux/MacOS to Linux with basic authentication with bad username over https should throw exception" -Skip:$IsWindows {
             $hostname = $LinuxHostName
             $User = $badUserName
             $password=$linuxPasswordString
@@ -59,20 +61,30 @@ Describe " PowerShell Remoting basic functional tests" -Tag @("CI") {
             }
             catch
             {
-                if($IsLinux -Or $IsOSX)
-                {
-                     # it maybe a error message issue on Linux/MacOS, just keep it now.
-                     $_.FullyQualifiedErrorId | Should be "2,PSSessionOpenFailed"
-                }
-                elseif($IsWindows)
-                {
-                    $_.FullyQualifiedErrorId | Should be "AccessDenied,PSSessionOpenFailed"
-                }
+                # it maybe a error message issue on Linux/MacOS, just keep it now.
+                $_.FullyQualifiedErrorId | Should be "2,PSSessionOpenFailed"
             }
-            
         }
 
-        It "Remoting from Windows/Linux/MacOS to Linux with basic authentication with bad password should throw exception" {
+        It "PowerShell from Windows/Linux/MacOS to Linux with basic authentication with bad username over https should throw exception" -Skip:($IsLinux -Or $IsOSX) {
+            $hostname = $LinuxHostName
+            $User = $badUserName
+            $password=$linuxPasswordString
+            $PWord = Convertto-SecureString $password -AsPlainText -Force
+            $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User,$PWord
+            $sessionOption = New-PSSessionOption -SkipCACheck -SkipRevocationCheck -SkipCNCheck
+            try
+            {
+                $mySession = New-PSSession -ComputerName $hostname -Credential $cred -Authentication Basic -UseSSL -SessionOption $sessionOption -ErrorAction Stop
+                Invoke-Command -Session $mySession {Get-Host}
+            }
+            catch
+            {
+                $_.FullyQualifiedErrorId | Should be "AccessDenied,PSSessionOpenFailed"
+            }
+        }
+
+        It "PowerShell from Linux/MacOS to Linux with basic authentication with bad password over https should throw exception" -Skip:$IsWindows {
             $hostname = $LinuxHostName
             $User = $LinuxUserName
             $PWord = Convertto-SecureString $badPassword -AsPlainText -Force
@@ -85,68 +97,75 @@ Describe " PowerShell Remoting basic functional tests" -Tag @("CI") {
             }
             catch
             {
-                if($IsLinux -Or $IsOSX)
-                {
-                     # it maybe a error message issue on Linux/MacOS, just keep it now.
-                     $_.FullyQualifiedErrorId | Should be "2,PSSessionOpenFailed"
-                }
-                elseif($IsWindows)
-                {
-                    $_.FullyQualifiedErrorId | Should be "AccessDenied,PSSessionOpenFailed"
-                }
+                # it maybe a error message issue on Linux/MacOS, just keep it now.
+                $_.FullyQualifiedErrorId | Should be "2,PSSessionOpenFailed"
             }
-            
+        }
+
+        It "PowerShell from Windows to Linux with basic authentication with bad password over https should throw exception" -Skip:($IsLinux -Or $IsOSX) {
+            $hostname = $LinuxHostName
+            $User = $LinuxUserName
+            $PWord = Convertto-SecureString $badPassword -AsPlainText -Force
+            $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
+            $sessionOption = New-PSSessionOption -SkipCACheck -SkipRevocationCheck -SkipCNCheck
+            try
+            {
+                $mySession = New-PSSession -ComputerName $hostname -Credential $cred -Authentication Basic -UseSSL -SessionOption $sessionOption -ErrorAction Stop
+                Invoke-Command -Session $mySession {Get-Host}
+            }
+            catch
+            {
+                $_.FullyQualifiedErrorId | Should be "AccessDenied,PSSessionOpenFailed"
+            }
         }
 
         #omicli pending when using bad username
-        It "Remoting from Windows/Linux/MacOS to Linux with basic authentication with winrm/omicli with bad username should throw exception" -Pending:($IsLinux -Or $IsOSX) {
+        It "Remoting from Linux/MacOS to Linux with basic authentication with omicli with bad username over https should throw exception" -Skip:($IsWindows) -Pending:($IsLinux -Or $IsOSX) {
             $hostname = $LinuxHostName
             $User = $badUserName
 
-            if($IsLinux -Or $IsOSX)
+            $result = /opt/omi/bin/omicli id -u $User -p $linuxPasswordString --auth Basic --hostname $hostname --port $Port --encryption https
+            $result | Should Match 'error'
+        }
+
+        It "WSMan from Windows to Linux with basic authentication with winrm with bad username over https should throw exception" -Skip:($IsLinux -Or $IsOSX) {
+            $hostname = $LinuxHostName
+            $User = $badUserName
+
+            try
             {
-                $result = /opt/omi/bin/omicli id -u $User -p $linuxPasswordString --auth Basic --hostname $hostname --port $Port --encryption https
-                $result | Should Match 'error'
-           }
-           elseif($IsWindows)
-           {
-                try
-                {
-                    winrm enumerate http://schemas.microsoft.com/wbem/wscim/1/cim-schema/2/OMI_Identify?__cimnamespace=root/omi -r:https://${hostname}:$Port -auth:Basic -u:$User -p:$linuxPasswordString -skipcncheck -skipcacheck -encoding:utf-8
-                }
-                catch
-                {
-                    $_.FullyQualifiedErrorId | Should be "NativeCommandError"
-                }
-           }
+                 winrm enumerate http://schemas.microsoft.com/wbem/wscim/1/cim-schema/2/OMI_Identify?__cimnamespace=root/omi -r:https://${hostname}:$Port -auth:Basic -u:$User -p:$linuxPasswordString -skipcncheck -skipcacheck -encoding:utf-8
+            }
+            catch
+            {
+                 $_.FullyQualifiedErrorId | Should be "NativeCommandError"
+            }
         }
 
         #omicli pending when using bad password
-        It "Remoting from Windows/Linux/MacOS to Linux with basic authentication with winrm/omicli with bad password should throw exception" -Pending:($IsLinux -Or $IsOSX) {
+        It "Remoting from Linux/MacOS to Linux with basic authentication with omicli with bad password over https should throw exception" -Skip:($IsWindows) -Pending:($IsLinux -Or $IsOSX) {
+            $hostname = $LinuxHostName
+            $User = $LinuxUserName
+            $result = /opt/omi/bin/omicli id -u $User -p $badPassword --auth Basic --hostname $hostname --port $Port --encryption https
+            $result | Should Match 'error'
+        }
+
+        It "WSMan from Windows to Linux with basic authentication with winrm with bad password over https should throw exception" -Skip:($IsLinux -Or $IsOSX) {
             $hostname = $LinuxHostName
             $User = $LinuxUserName
             
-            if($IsLinux -Or $IsOSX)
+            try
             {
-                $result = /opt/omi/bin/omicli id -u $User -p $badPassword --auth Basic --hostname $hostname --port $Port --encryption https
-                $result | Should Match 'error'
-           }
-           elseif($IsWindows)
-           {
-                try
-                {
-                    winrm enumerate http://schemas.microsoft.com/wbem/wscim/1/cim-schema/2/OMI_Identify?__cimnamespace=root/omi -r:https://${hostname}:$Port -auth:Basic -u:$User -p:$badPassword -skipcncheck -skipcacheck -encoding:utf-8
-                }
-                catch
-                {
-                    $_.FullyQualifiedErrorId | Should be "NativeCommandError"
-                }
-           }
-            
+                winrm enumerate http://schemas.microsoft.com/wbem/wscim/1/cim-schema/2/OMI_Identify?__cimnamespace=root/omi -r:https://${hostname}:$Port -auth:Basic -u:$User -p:$badPassword -skipcncheck -skipcacheck -encoding:utf-8
+            }
+            catch
+            {
+                $_.FullyQualifiedErrorId | Should be "NativeCommandError"
+            }
         }
 
         #Skip Windows to Windows because of not support.
-        It "Remoting from Linux/MacOS to Windows with basic authentication should work" -Skip:($IsWindows) {
+        It "Remoting from Linux/MacOS to Windows with basic authentication over http should work" -Skip:($IsWindows) {
             $hostname = $WindowsHostName
             $User = $WindowsUserName
             $PWord = Convertto-SecureString $windowsPasswordString -AsPlainText -Force
@@ -163,19 +182,15 @@ Describe " PowerShell Remoting basic functional tests" -Tag @("CI") {
             Get-PSSession|Remove-PSSession
         }
 
-        #Skip Windows to Windows because of not support.
-        It "Remoting from Linux/MacOS to Windows with basic authentication by omicli should work" -Skip:($IsWindows) {
+        It "Remoting from Linux/MacOS to Windows with basic authentication by omicli over http should work" -Skip:($IsWindows) {
             $hostname = $WindowsHostName
             $User = $WindowsUserName
             $HTTPPort=5985
-            if($IsLinux -Or $IsOSX)
-            {
-                $result = /opt/omi/bin/omicli ei root/cimv2 Win32_SystemOperatingSystem -u $User -p $windowsPasswordString --auth Basic --hostname $hostname --port $HTTPPort --encryption none
-                $result|Should Not BeNullOrEmpty
-            }
+            $result = /opt/omi/bin/omicli ei root/cimv2 Win32_SystemOperatingSystem -u $User -p $windowsPasswordString --auth Basic --hostname $hostname --port $HTTPPort --encryption none
+            $result|Should Not BeNullOrEmpty
         }
 
-        It "Remoting from Windows/Linux/MacOS to Linux with negotiate authentication should work" {
+        It "PowerShell from Windows/Linux/MacOS to Linux with negotiate authentication over https should work" {
             $hostname = $LinuxHostName
             $User = $LinuxUserName
             $password=$linuxPasswordString
@@ -193,7 +208,7 @@ Describe " PowerShell Remoting basic functional tests" -Tag @("CI") {
             Get-PSSession|Remove-PSSession
         }
 
-        It "Remoting from Windows/Linux/MacOS to Linux with negotiate authentication with winrm/omicli should work" {
+        It "Remoting from Linux/MacOS to Linux with negotiate authentication with omicli over https should work" -SKip:($IsWindows) {
             $hostname = $LinuxHostName
             $User = $LinuxUserName
             if($IsLinux -Or $IsOSX)
@@ -201,16 +216,18 @@ Describe " PowerShell Remoting basic functional tests" -Tag @("CI") {
                 $result = /opt/omi/bin/omicli id -u "$hostname\$User" -p $linuxPasswordString --auth NegoWithCreds --hostname $hostname --port $Port --encryption https
                 $result|Should Not BeNullOrEmpty
             }
-            elseif($IsWindows)
-            {
-                $result = winrm enumerate http://schemas.microsoft.com/wbem/wscim/1/cim-schema/2/OMI_Identify?__cimnamespace=root/omi -r:https://${hostname}:$Port -auth:negotiate -u:"$hostname\$User" -p:$linuxPasswordString -skipcncheck -skipcacheck -encoding:utf-8
-                # "$result | Should Not BeNullOrEmpty" not works here, it is a Pester bug, so just use Length verificaiton now
-                $result.Length|Should BeGreaterThan 1
-            }
+        }
+
+        It "WSMan from Windows to Linux with negotiate authentication with winrm over https should work" -SKip:($IsLinux -Or $IsOSX) {
+            $hostname = $LinuxHostName
+            $User = $LinuxUserName
+            $result = winrm enumerate http://schemas.microsoft.com/wbem/wscim/1/cim-schema/2/OMI_Identify?__cimnamespace=root/omi -r:https://${hostname}:$Port -auth:negotiate -u:"$hostname\$User" -p:$linuxPasswordString -skipcncheck -skipcacheck -encoding:utf-8
+            # "$result | Should Not BeNullOrEmpty" not works here, it is a Pester bug, so just use Length verificaiton now
+            $result.Length|Should BeGreaterThan 1
         }
 
         #Skip Windows to Windows because of not support.
-        It "Remoting from Linux/MacOS to Windows with negotiate authentication should work" -Skip:($IsWindows) {
+        It "PowerShell from Linux/MacOS to Windows with negotiate authentication over http should work" -Skip:($IsWindows) {
             $hostname = $WindowsHostName
             $User = $WindowsUserName
             $PWord = Convertto-SecureString $windowsPasswordString -AsPlainText -Force

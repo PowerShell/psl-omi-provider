27ef27a724ebbc7d4032a7ebf6faa2884ef8cc95
Dan Travison <dantra@ntdev.microsoft.com>
2018-08-30 10:38:17 -0700

    * Add SupportsSSH, SupportsNTLM, and IsSupportedEnvironment to TestVariables in Get-TestVariables * Temporarily refactor SSH tests to only create the PSSession once to avoid long connectivity timeout issues * Report all errors in Get-TestVariables * Modify -Skip logic to incorporate appropriate test variables.

--- test/LabTests/LinuxToWindows.tests.ps1
+++ test/LabTests/LinuxToWindows.tests.ps1
@@ -23,30 +23,52 @@
                 Fatal error if not found
     Password - the value of env:PSRPPassword
                Fatal error if not found
-    KeyFileName - the path to the private key to use for ssh connections
+    KeyFilePath - the path to the private key to use for ssh connections
                SSH tests are skipped if not set.
+    IsSupportedEnviroment - $true if the current system supports these tests
+    SupportsNTLM - $true if the system supports NTLM authentication.
+                 - Currently, MacOS does not.
 #>
 function Get-TestVariables([ref] [string] $error)
 {
     $vars = @{}
+    $err = [System.Text.StringBuilder]::new()
 
     do
     {
         if (!(Test-Path -Path env:PSRPHost))
         {
-            $error.Value = '$env:PSRPHost was not found'
-            break
+            $null = $err.AppendLine('$env:PSRPHost was not found')
         }
 
         if (!(Test-Path -Path env:PSRPUserName))
         {
-            $error.Value = '$env:PSRPUserName was not found'
-            break
+            $null = $err.AppendLine('$env:PSRPUserName was not found')
         }
 
         if (!(Test-Path -Path env:PSRPPassword))
         {
-            $error.Value = '$env:PSRPPassword was not found'
+            $null = $err.AppendLine('$env:PSRPPassword was not found')
+        }
+
+        if (Test-Path -Path env:PSRPKeyFilePath)
+        {
+            $vars.Add('SupportsSSH', [bool] $true)
+            $vars.Add('KeyFilePath', $env:PSRPKeyFilePath)
+            if (!(Test-Path -Path $env:PSRPKeyFilePath))
+            {
+                $null = $err.AppendLine("$env:PSRPKeyFilePath was not found")
+            }
+        }
+        else
+        {
+            # Must have a private key file to avoid prompting for a password
+            $vars.Add('SupportsSSH', [bool] $false)
+        }
+
+        if ($err.Length -gt 0)
+        {
+            $error.Value = $err.ToString()
             break
         }
 
@@ -54,15 +76,8 @@ function Get-TestVariables([ref] [string] $error)
         $vars.Add('UserName', $env:PSRPUserName)
         $vars.Add('Credential', [PSCredential]::new($env:PSRPUserName,  [System.Net.NetworkCredential]::new('', $env:PSRPPassword).SecurePassword))
 
-        if (Test-Path -Path env:PSRPKeyFilePath)
-        {
-            $vars.Add('KeyFilePath', $env:PSRPKeyFilePath)
-            $vars.Add('SkipSSH', $false)
-        }
-        else
-        {
-            $vars.Add('SkipSSH', $true)
-        }
+        $vars.Add('IsSupportedEnvironment', [bool] $IsLinux)
+        $vars.Add('SupportsNTLM', [bool] (!$IsMacOS -and $IsLinux))
 
     } while ($false)
 
@@ -73,8 +88,14 @@ function Get-TestVariables([ref] [string] $error)
 #region Test Implementation
 
 <#
-    Creates a binary file of the specified size
-    on the test drive.
+.Synopsis
+    Creates a binary file of the specified size on the test drive.
+.Parameter File
+    The fully qualified binary file to create.
+    If the file already exists, it will be removed.
+.Parameter Size
+    The size, in bytes, of the file.
+.Notes
     Used by Copy-Item tests
 #>
 function New-TestFile
@@ -95,8 +116,18 @@ function New-TestFile
 }
 
 <#
+.Synopsis
     Provides the Copy-Item test over a PSSession.
-    Called by tests specific to each connection type.
+.Parameter Session
+    The PSSession to use as the target of the copy
+.Parameter TestVars
+    The Test variables shared by all tests.
+.Parameter
+    The name of the file to create.
+.Description
+    The test creates a file in the root of the Pester test drive
+    then copies it to the home directory on the target system.
+    After the copy completes, it verifies the file exists.
 #>
 function Test-CopyItem
 {
@@ -127,8 +158,11 @@ function Test-CopyItem
 }
 
 <#
-    Provides the Invoke-Command test over a PSSession
-    Called by tests specific to each connection type.
+.Synopsis
+    Verifies a session is valid by querying the $PSSenderInfo variable on the target system
+    using Invoke-Command
+.Parameter session
+    The PSSession to the target system.
 #>
 function Test-InvokeCommand
 {
@@ -144,6 +178,26 @@ function Test-InvokeCommand
 
 #endregion Test Implementation
 
+<#
+    Provides connectivity tests for PSRP/Linux
+    to a target Windows Server using Negotiate, SSL, and OpenSSH
+    connections.
+
+    Error reporting:
+
+    Some of the BeforeAll and BeforeEach blocks perform non-trivial
+    setup for tests. Typically, when these fail, tests are run but are not
+    in a predictable state and failures often do not reflect the root cause.
+    To handle this, an error message is created for reporting by the individual
+    tests using Should -BeNullOrEmpty.
+
+    For example, if a PSSession cannot be created in BeforeEach, a $ContextError is populated
+    in BeforeEach that indicates the session could not be created the target machine. The result
+    is BeforeEach error is reported and containing tests report a consistent error.
+
+    If an error occurs in the Describe-level BeforeAll, a DescribeError is populated and subsequent
+    Context/It statements use this as the failure text.
+#>
 Describe 'Connect to Windows Server from Linux' {
 
     BeforeAll {
@@ -155,15 +209,16 @@ Describe 'Connect to Windows Server from Linux' {
         [System.Management.Automation.Runspaces.PSSession] $session = $null
     }
 
-    AfterEach {
-        Get-PSSession | Remove-PSSession
-    }
-
     Context 'Connect With -Authentication Negotiate (NTLM)' {
+
+        BeforeAll {
+            [bool] $Skip = !($TestVariables.IsSupportedEnvironment -and $TestVariables.SupportsNTLM)
+        }
 
         BeforeEach {
             [string] $ContextError = $DescribeError
-            if (!$ContextError)
+
+            if (!$Skip -and !$ContextError)
             {
                 $session = New-PSSession -ComputerName $TestVariables.Hostname -Credential $TestVariables.Credential -Authentication Negotiate
                 if ($null -eq $session)
@@ -174,20 +229,20 @@ Describe 'Connect to Windows Server from Linux' {
         }
 
         AfterEach {
-            $ContextError = ''
+            Get-PSSession | Remove-PSSession
         }
 
-        It 'Verifies New-PSSession -Authentication Negotiate connects with no errors' {
+        It 'Verifies New-PSSession -Authentication Negotiate connects with no errors' -Skip:$Skip {
             $ContextError | Should -BeNullOrEmpty
             $session | Should -Not -Be $null
         }
 
-        It 'Verifies Enter-PSSession -Authentication Negotiate succeeds' {
+        It 'Verifies Enter-PSSession -Authentication Negotiate succeeds' -Skip:$Skip {
             $ContextError | Should -BeNullOrEmpty
             Test-InvokeCommand -Session $session
         }
 
-        It 'Verifies content can be copied to a Negotiate PSSession' {
+        It 'Verifies content can be copied to a Negotiate PSSession' -Skip:$Skip {
             $ContextError | Should -BeNullOrEmpty
             Test-CopyItem -Session $session -TestFileName 'CopyItemOverNTLM.bin' -TestVars $TestVariables
         }
@@ -195,9 +250,14 @@ Describe 'Connect to Windows Server from Linux' {
 
     Context 'Connect with -UseSSL' {
 
+        BeforeAll {
+            [bool] $Skip = !$TestVariables.IsSupportedEnvironment
+        }
+
         BeforeEach {
             [string] $ContextError = $DescribeError
-            if (!$ContextError)
+
+            if (!$Skip -and !$ContextError)
             {
                 $session = New-PSSession -ComputerName $TestVariables.Hostname -Credential $TestVariables.Credential -Authentication Basic -UseSSL
                 if ($null -eq $session)
@@ -208,30 +268,35 @@ Describe 'Connect to Windows Server from Linux' {
         }
 
         AfterEach {
-            $ContextError = ''
+            Get-PSSession | Remove-PSSession
         }
 
-        It 'Verifies New-PSSession -UseSSL connects with no errors' {
+        It 'Verifies New-PSSession -UseSSL connects with no errors' -Skip:$Skip {
             $ContextError | Should -BeNullOrEmpty
             $session | Should -Not -Be $null
         }
 
-        It 'Verifies Invoke-Command succeeds with a -UseSSL PSSession' {
+        It 'Verifies Invoke-Command succeeds with a -UseSSL PSSession' -Skip:$Skip {
             $ContextError | Should -BeNullOrEmpty
             Test-InvokeCommand -Session $session
         }
 
-        It 'Verifies content can be copied to a -UseSSL PSSession' {
+        It 'Verifies content can be copied to a -UseSSL PSSession' -Skip:$Skip {
             $ContextError | Should -BeNullOrEmpty
             Test-CopyItem -Session $session -TestFileName 'CopyItemOverNTLM.bin' -TestVars $TestVariables
         }
     }
 
     Context 'Connect with SSH' {
-        BeforeEach {
+
+        BeforeAll {
+            [bool] $Skip = !($TestVariables.IsSupportedEnvironment -and $TestVariables.SupportsSSH)
             [string] $ContextError = $DescribeError
-            if (!$TestVariables.SkipSSH -and !$DescribeError)
+            if (!$Skip -and !$ContextError)
             {
+                # ISSUE: Only creating the session once for all tests due to
+                # recurring connection timeout issues with the target server.
+                # Move this to BeforeEach once this is resolved.
                 $session = New-PSSession -HostName $TestVariables.Hostname -UserName $TestVariables.UserName -KeyFilePath $TestVariables.KeyFilePath
                 if ($null -eq $session)
                 {
@@ -240,21 +305,35 @@ Describe 'Connect to Windows Server from Linux' {
             }
         }
 
-        AfterEach {
-            $ContextError = ''
+        BeforeEach {
+        <# Re-enable once connection time issues resolved with the target Windows Server 2016
+            [string] $ContextError = $DescribeError
+            if (!$Skip -and !$ContextError)
+            {
+                $session = New-PSSession -HostName $TestVariables.Hostname -UserName $TestVariables.UserName -KeyFilePath $TestVariables.KeyFilePath
+                if ($null -eq $session)
+                {
+                    $ContextError = "Could not create SSH PSSession to $($TestVariables.HostName)"
+                }
+            }
+        #>
         }
 
-        It 'Verifies New-PSSession over SSH connects with no errors' -Skip:$TestVariables.SkipSSH {
+        AfterAll {
+            Get-PSSession | Remove-PSSession
+        }
+
+        It 'Verifies New-PSSession over SSH connects with no errors' -Skip:$Skip {
             $ContextError | Should -BeNullOrEmpty
             $session | Should -Not -Be $null
         }
 
-        It 'Verifies Invoke-Command succeeds with an SSH PSSession' -Skip:$TestVariables.SkipSSH {
+        It 'Verifies Invoke-Command succeeds with an SSH PSSession' -Skip:$Skip {
             $ContextError | Should -BeNullOrEmpty
             Test-InvokeCommand -Session $session
         }
 
-        It 'Verifies content can be copied to an SSH PSSession'  -Skip:$TestVariables.SkipSSH {
+        It 'Verifies content can be copied to an SSH PSSession'  -Skip:$Skip {
             $ContextError | Should -BeNullOrEmpty
             Test-CopyItem -Session $session -TestFileName 'CopyItemOverNTLM.bin' -TestVars $TestVariables
         }
